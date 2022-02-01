7d2ae9e0e535687098144e6c4467e72e63f91957
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-02-01 14:26:31 +0200

    JS užduotys

---
+++ 2022-02-01-AS/assets/js/index.js
@@ -0,0 +1,59 @@
+document.getElementById("submit").addEventListener("click", function (e) {
+  var reiksme = document.getElementById("skc").value;
+  document.getElementById("rez").innerHTML = "<h1>" + reiksme + "</h1>";
+});
+const d = document.querySelector.bind(document);
+
+// window.addEventListener('scroll', () => {
+//   console.log(window.pageYOffset)
+// })
+
+function randomSkaicius(x, y) {
+  return Math.round(Math.random() * (x - y + 1) + y);
+}
+function randomSkaiciusRound(x, y) {
+  return (Math.round(Math.random() * (x - y + 1) + y) * 100) / 10000;
+}
+
+document.getElementById("test-id").innerHTML = randomSkaicius(100, 1);
+
+let a1 = randomSkaiciusRound(10000, 1);
+let a2 = randomSkaiciusRound(10000, 1);
+let a3 = randomSkaiciusRound(10000, 1);
+
+function color1() {
+  if (a1 >= 70) {
+    d(".test-c1").style.color = "green";
+  } else if (a1 >= 30 && a1 < 70) {
+    d(".test-c1").style.color = "orange";
+  } else {
+    d(".test-c1").style.color = "red";
+  }
+}
+
+function color2() {
+  if (a2 >= 70) {
+    d(".test-c2").style.color = "green";
+  } else if (a2 >= 30 && a2 < 70) {
+    d(".test-c2").style.color = "orange";
+  } else {
+    d(".test-c2").style.color = "red";
+  }
+}
+
+function color3() {
+  if (a3 >= 70) {
+    d(".test-c3").style.color = "green";
+  } else if (a3 >= 30 && a3 < 70) {
+    d(".test-c3").style.color = "orange";
+  } else {
+    d(".test-c3").style.color = "red";
+  }
+}
+
+d(".test-c1").innerHTML = a1;
+color1();
+d(".test-c2").innerHTML = a2;
+color2();
+d(".test-c3").innerHTML = a3;
+color3();
