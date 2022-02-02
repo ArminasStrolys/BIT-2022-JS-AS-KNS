59a85ce9c540a53434be9f1d77bbf48537714a75
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-02-02 09:33:31 +0200

    Update index.js

--- Project-1-landing-page/assets/js/index.js
+++ Project-1-landing-page/assets/js/index.js
@@ -1,11 +1,12 @@
 let position;
+const $ = document.querySelector.bind(document);
 
 window.addEventListener("scroll", function () {
     position = window.scrollY;
   if (position >= 203) {
-    document.querySelector(".navbar").classList.add("navbar-2")
+    $(".navbar").classList.add("navbar-2")
   } else if (position < 203) {
-    document.querySelector(".navbar").classList.remove("navbar-2")
+    $(".navbar").classList.remove("navbar-2")
   }
   console.log(position);
 });
