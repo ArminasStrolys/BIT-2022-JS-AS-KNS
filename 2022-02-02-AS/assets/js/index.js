b40e9a93ce510863d5eee8d909c632239ff374ee
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-02-02 15:31:31 +0200

    Antra užduotis

--- 2022-02-02-AS/assets/js/index.js
+++ 2022-02-02-AS/assets/js/index.js
@@ -1,20 +1,18 @@
-const $ = document.querySelectorAll.bind(document);
+const p0 = document.querySelectorAll.bind(document);
 
-function getRandomInt(min, max) {
+function randomizer(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
   
 }
-function rand1(){
+function uzd1(){
   let x
   let y0 = 0
   let z1 = 0
   let c2 = 0
   
-
-x = $('span')[0].innerHTML = getRandomInt(0, 3)
-console.log(x)
+x = document.querySelectorAll('span')[0].innerHTML = randomizer(0, 3)
 if (x == 0) {
   y0 += 1
 } else if (x == 1){
@@ -23,7 +21,7 @@ if (x == 0) {
   c2 +=1
 }
 
-x = $('span')[1].innerHTML = getRandomInt(0, 3)
+x = p0('span')[1].innerHTML = randomizer(0, 3)
 if (x == 0) {
   y0 += 1
 } else if (x == 1){
@@ -32,7 +30,7 @@ if (x == 0) {
   c2 +=1
 }
 
-x = $('span')[2].innerHTML = getRandomInt(0, 3)
+x = p0('span')[2].innerHTML = randomizer(0, 3)
 if (x == 0) {
   y0 += 1
 } else if (x == 1){
@@ -41,7 +39,7 @@ if (x == 0) {
   c2 +=1
 }
 
-x = $('span')[3].innerHTML = getRandomInt(0, 3)
+x = p0('span')[3].innerHTML = randomizer(0, 3)
 if (x == 0) {
   y0 += 1
 } else if (x == 1){
@@ -50,9 +48,20 @@ if (x == 0) {
   c2 +=1
 }
 
-  $('span')[4].innerHTML = y0
-  $('span')[5].innerHTML = z1
-  $('span')[6].innerHTML = c2
+  p0('span')[4].innerHTML = y0
+  p0('span')[5].innerHTML = z1
+  p0('span')[6].innerHTML = c2
 }
 
+// ----------------------------------------------------
 
+function uzd2(){
+let m
+m = p0('span')[7].innerHTML = randomizer(1, 7)
+p0('i')[m].innerHTML = `ELEMENTAS YRA ČIA - H${m}`
+setTimeout(function() {
+  location.reload();
+}, 1000);
+console.log(m)
+console.log(p0(`.h${m}`).innerHTML = 'Hello')
+}
