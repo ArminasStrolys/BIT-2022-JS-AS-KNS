3cd097aed9c9c7fe3a31745cfbd64f95578022b3
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-02-08 16:03:33 +0200

    Pirmos 5 užduotys

--- Kestucio-uzdaviniu-viktorina/assets/js/index.js
+++ Kestucio-uzdaviniu-viktorina/assets/js/index.js
@@ -2,116 +2,181 @@ function rndNum(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-// -----------------------MONETOS--------------------------------------------
+// ----------------------------- 1 -----------------------------
 
-// let x = 0;
-// let y = 0;
-// // 0 herbas (x), 1 skaicius (y)
+/*
+Žinomas skaičius n. Parašykite programą, kuri nustatytų ar skaičius
+yra blogas ar geras? Neigiamas – „Blogas“, teigiamas „Geras“.
+Pasitikrinkite: Kai n = 20, turėtume gauti: Skaičius geras.
+*/
 
-// while (true) {
-//   let a = rndNum(0, 1);
+// let x = rndNum(-100, 100);
+// console.log("Skaičius " + x);
 
-//   if (a == 0) {
-//     x++;
-//   } else y++;
-
-//   console.log('y ' + y)
-//   console.log('x ' + x)
-//   console.log('a ' + a)
-//   console.log('------')
-
-//   if (y == 0 && x == 3) {
-//     console.log(`Iškrito herbas 3 kartus iš eilės!!!!`);
-//     break;
-//   }
-//   if (x == 3) {
-//     console.log(`Iškrito herbas 3 kartus`);
-//     break;
-//   }
-// //   else {
-// //     console.log(`Iškrito herbas`);
-// //     break;
-// //   }
+// if (x >= 0) {
+//   console.log("Geras skaičius");
+// } else {
+//   console.log("Blogas skaičius");
 // }
 
-// -----------------------MAZAIS VINIMIS--------------------------------------------
+// ----------------------------- 2 -----------------------------
+/*
+Mokinys eina gatve ir prieina sankryžą reguliuojamą šviesoforu.
+Sudarykite programą, kuri nustatytu, kaip turi pasielgti mokinys?
+Žinome šviesoforo spalvos kodą n, 1-žalia, 2-geltona, 3-raudona.
+Pasitikrinkite: Kai n= 2, turėtume gauti: Palaukite.
+*/
 
-// let vinys = 85;
-// let gylis = 0;
-// let vinKiekis = 0;
-// let ciklas = 0;
-// let smugiai = 0;
+// let n = rndNum(1, 3);
+//  console.log("Skaičius " + n);
 
-// while (vinKiekis <= 5) {
-//   let mazas = rndNum(5, 20);
-//   gylis += mazas;
-//     smugiai++
-//   if (gylis >= vinys) {
-//     ciklas++;
-//     // console.log(vinKiekis);
-//   }
-//   if (ciklas == 1) {
-//     gylis = 0;
-//     ciklas = 0;
-//     vinKiekis++;
-//   }
-//   if (vinKiekis >= 5) {
-//     console.log("------------------------");
-//     console.log("mazas smugis " + mazas + "mm");
-//     console.log("Vinių jau sukalta = " + vinKiekis);
-//     console.log("gylis " + gylis);
-//     console.log("Smūgių reikėjo = " + smugiai);
-//     break;
-//   }
-//   console.log("------------------------");
-//   console.log("mazas smugis " + mazas + "mm");
-//   console.log("Vinių jau sukalta = " + vinKiekis);
-//   console.log("gylis " + gylis);
-//   console.log("Smūgių reikėjo = " + smugiai);
+//  if (n == 1){
+//   console.log("Žalia - eiti " + n);
+//  } else if (n == 2){
+//   console.log("Geltona - palaukti " + n);
+//  } else {
+//   console.log("Raudona - sustoti " + n);
+//  }
+
+// ----------------------------- 3 -----------------------------
+/*
+Gintautas planuoja persikraustyti į naujus namus ir jam reikia sudėti
+visas turimas knygas į dėžes. Gintautas turi d dėžių ir k knygų.
+Parašykite programą, kuri apskaičiuotų, ar Gintauto turimos knygos
+tilps į dėžes, jei į vieną dėžę telpa po n knygų. Pasitikrinkite:
+Kai d=2, k=8, n=5 tuomet ekrane turi būti rodomas pranešimas: Knygos telpa
+į dėžes; kai d=3, k=18, n=5 tuomet ekrane turi būti rodomas pranešimas:
+Knygos netelpa į dėžes;
+*/
+
+// let k = rndNum(10, 30); // knygos
+// console.log('k = ' + k)
+// let d = rndNum(2, 6); // dėžės
+// console.log('d = ' + d)
+// let n = rndNum(4, 8); // telpa knygų dėžėje
+// console.log('n = ' + n)
+
+// if ((d * n) > k){
+// console.log('Knygos telpa į dėžes')
+// } else {
+//   console.log('Knygos netelpa į dėžes')
 // }
 
+// ----------------------------- 4 -----------------------------
+/*
+Skaičius skaitomas skaitmenimis iš abiejų galų vienodai vadinamas polindromu.
+Pvz.: 121. Žinomas 4-klis natūralus skaičius n. Nustatykite ar skaičius yra
+polindromas. Pasitikrinkite: Kai n = 1221 , turėtume gauti: Skaičius
+polindromas; kai n=1223 tada Skaičius nepolindromas
+*/
 
-// -------------------------DIDELIAIS VINIMIS---------------------------------------
+// let n = 1221
+// console.log('n = ' + n)
+// let a = Math.floor(n / 1000)
+// console.log('a = ' + a)
+// let b = Math.floor(n / 100) % 10
+// console.log('b = ' + b)
+// let c = Math.floor(n / 10) % 10
+// console.log('c = ' + c)
+// let d = n % 10
+// console.log('d = ' + d)
 
-let vinys = 85;
-let gylis = 0;
-let vinKiekis = 0;
-let ciklas = 0;
-let smugiai = 0;
+// if (a == d && b == c){
+//   console.log('Skaičius yra polindromas')
+// } else {
+//   console.log('Skaičius nėra polindromas')
+// }
 
+// ----------------------------- 5 -----------------------------
+/*
+Loterijos bilietą sudaro šešiaženklis numeris. Loterijoje laimi tas
+bilietas, kurio numerio skaitmenų suma dalosi iš 4. Nustatykite, ar
+loterijos bilietas n yra laimingas?. Pasitikrinkite: Kai n = 222442,
+turėtume gauti: Bilietas laimingas
+*/
 
-while (vinKiekis <= 5) {
-  let didelis = rndNum(20, 30);
-  let tikimybe = rndNum(0, 1)
-  if (tikimybe == 1){
-    smugiai++
-    console.log("****************");
-    console.log("Nepataikė į vinį");
-    console.log("****************");
-      continue
-  }
-  gylis += didelis;
-    smugiai++
-  if (gylis >= vinys) {
-    ciklas++;
+// let n = rndNum(100000, 999999); // skaičius
+// // let n = 222442 // skaičius
+// console.log('n = ' + n)
 
-  }
-  if (ciklas == 1) {
-    gylis = 0;
-    ciklas = 0;
-    vinKiekis++;
-  }
-  if (vinKiekis >= 5) {
-    console.log("------------------------");
-    console.log("Didelis smūgis " + didelis + "mm");
-    console.log("Vinių jau sukalta = " + vinKiekis);
-    console.log("Gylis " + gylis);
-    console.log("Smūgių reikėjo = " + smugiai);
-    break;
-  }
-  console.log("------------------------");
-  console.log("Didelis smūgis " + didelis + "mm");
-  console.log("Vinių jau sukalta = " + vinKiekis);
-  console.log("Gylis " + gylis);
-  console.log("Smūgių reikėjo = " + smugiai);
-}
+// let n1 = Math.floor(n / 100000)
+// console.log('n1 = ' + n1)
+// let n2 = Math.floor(n / 10000) % 10
+// console.log('n2 = ' + n2)
+// let n3 = Math.floor(n / 1000) % 10
+// console.log('n3 = ' + n3)
+// let n4 = Math.floor(n / 100) % 10
+// console.log('n4 = ' + n4)
+// let n5 = Math.floor(n / 10) % 10
+// console.log('n5 = ' + n5)
+// let n6 = n % 10
+// console.log('n6 = ' + n6)
+
+// let sum = n1 + n2 + n3 + n4 + n5 + n6
+
+// if (sum % 4 == 0){
+//   console.log('Bilietas laimingas')
+// } else {
+//   console.log('Bilietas nelaimingas')
+// }
+
+// ----------------------------- 6 -----------------------------
+/*
+
+*/
+
+// ----------------------------- 7 -----------------------------
+/*
+
+*/
+
+// ----------------------------- 8 -----------------------------
+/*
+
+*/
+
+// ----------------------------- 9 -----------------------------
+/*
+
+*/
+
+// ----------------------------- 10 -----------------------------
+/*
+
+*/
+
+// ----------------------------- 11 -----------------------------
+/*
+
+*/
+
+// ----------------------------- 12 -----------------------------
+/*
+
+*/
+
+// ----------------------------- 13 -----------------------------
+/*
+
+*/
+
+// ----------------------------- 14 -----------------------------
+/*
+
+*/
+
+// ----------------------------- 15 -----------------------------
+/*
+
+*/
+
+// ----------------------------- 16 -----------------------------
+/*
+
+*/
+
+// ----------------------------- 17 -----------------------------
+/*
+
+*/
+
