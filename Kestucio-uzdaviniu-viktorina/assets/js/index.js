2df8b5add8ccdb25b4860b6e01942f47c4829137
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-02-08 22:34:19 +0200

    Užduotys iki 15

--- Kestucio-uzdaviniu-viktorina/assets/js/index.js
+++ Kestucio-uzdaviniu-viktorina/assets/js/index.js
@@ -230,27 +230,117 @@ Petras į pamoką pavėluos; jei v1 = 8, m1 = 29, m2 = 23, v = 9, m = 5,
 turi būti: Petras į pamoką nepavėluos.
 */
 
+// let v1 = 8  // Valanda išėjus iš namų
+// let m1 = 29 // Valandos minutės išėjus iš namų
+// let v = 9   // Valanda, kada prasideda pamoka
+// let m = 5   // Valandos minutės, kada prasideda pamoka
+// let m2 = 23 // Kelionės trukmė (min.)
 
+// let x // Kai minutės viršys valandą
+
+// x = m1 + m2
+// if (x >= 60){
+//   v1++
+//   m1 = x - 60
+// }
+// console.log(x)
+// console.log(m1)
+// console.log(v1)
+
+// if (v1 > v || v1 >= v && m > m1){
+// console.log('Petras pavėluos į pamoką')
+// } else {
+//   console.log('Petras nepavėluos į pamoką')
+// }
 
 // ----------------------------- 12 -----------------------------
 /*
-
+Yra n degtukų. Parašykite programą, kuri nustatytų, ar iš tų degtukų
+galima sudėti lygiakraštį trikampį. Dėliojamai figūrai turi būti panaudoti
+visi degtukai. Degtukų laužyti negalima. Pasitikrinkite: jei n = 6,
+turi būti: Trikampį sudėlioti galima. jei n = 4, turi būti: Trikampį
+sudėlioti negalima.
 */
+
+// let n = 12
+
+// if (n % 3 == 0){
+//   console.log('Trikampis išeina')
+// } else {
+//   console.log('Trikampis neišeina')
+// }
 
 // ----------------------------- 13 -----------------------------
 /*
-
+Pirmosios olimpinės žaidynės įvyko 1896 metais ir toliau organizuojamos
+kas ketveri metai. Jei žaidynės neįvyksta, tie metai vis tiek laikomi
+olimpiniais, o žaidynėms skiriamas eilės numeris. Parašykite programą, kuri
+surastų m-ųjų metų olimpinių žaidynių numerį n. Jei metai neolimpiniai, turi
+būti spausdinama „Metai neolimpiniai“. Pasitikrinkite. Kai m = 1904, turi
+būti spausdinama: n = 3. Kai m = 2005, turi būti spausdinama:
+Metai neolimpiniai.
 */
+// let x = [1916, 1940, 1944]; // Nevykusių olimpinių metai
+
+// let n = 0;
+
+// for (let i = 1896; i < 2024; i++) {
+//   if (i % 4 == 0 && i !== 1916) {
+//     n++
+//     console.log("Metai olimpiniai = " + i + ' N= ' + n);
+//   } else if (i == 1916 || i == 1940 || i == 1944){
+//     n++
+//   } else {
+//     console.log("Metai neolimpiniai = " + i + ' N= ' + n);
+//   }
+// }
+// console.log(`Žaidynės vyko ${1896 + (n * 4)}`);
 
 // ----------------------------- 14 -----------------------------
 /*
-
+Žinomi kvadratinės lygties ax2+bx+c=0 koeficientai a, b, c. Parašykite
+programą, kuri rastų lygties sprendinius. Pasitikrinkite:a=1, b=2, c=1,
+kompiuterio ekrane turi būti rodomas rezultatas: vienas sprendinys
+1.00 a=1, b=4, c=3, kompiuterio ekrane turi būti rodomas rezultatas:
+du sprendiniai 3.00 1.00 a=1, b=3, c=4, kompiuterio ekrane turi būti
+rodomas rezultatas: nėra sprendinių
 */
+
+let a = 1;
+let b = 2;
+let c = 1;
+let x;
+let x1;
+let x2;
+let d;
+
+d = (b ** 2) - (4 * a * c);
+
+if (d < 0) {
+  console.log("sprendinių nėra");
+} else if (d == 0) {
+  x = -b / (2 * a);
+  console.log("X = " + x);
+} else if (d > 0) {
+  x1 = (-b + Math.sqrt(d)) / (2 * a);
+  x2 = (-b - Math.sqrt(d)) / (2 * a);
+  console.log("X1 = " + x1);
+  console.log("X2 = " + x2);
+}
 
 // ----------------------------- 15 -----------------------------
 /*
-
+Saulius sutaupė c centų ir už juos nusprendė nusipirkti ledų. Parduotuvėje
+porcija ledų kainuoja p centų. Parašykite programą, kuri suranda, kiek
+porcijų ledų k nusipirks Saulius ir kiek centų m dar liks. Pasitikrinkite
+Kai c=50, p=20, turi būti : Saulius nusipirks 2 porcijas, liks centų 10
 */
+
+// let c = 50 // Centai
+// let p = 20 // Porcijos kaina
+// let k = 0 // Porcijos
+// let g
+
 
 // ----------------------------- 16 -----------------------------
 /*
