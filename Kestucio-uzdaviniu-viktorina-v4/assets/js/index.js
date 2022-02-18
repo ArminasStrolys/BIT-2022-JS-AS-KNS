0d90a45c87af55ef575d6d90a5758d661565af12
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-02-18 15:25:41 +0200

    Užduotys

---
+++ Kestucio-uzdaviniu-viktorina-v4/assets/js/index.js
@@ -0,0 +1,168 @@
+const $ = document.querySelectorAll.bind(document);
+
+function rndNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// ----------------------------- 1 -----------------------------
+/*
+Parašykite funkciją, kurios argumentas būtų tekstas,
+kuris yra įterpiamas į h1 tagą.
+*/
+
+// const texting = (a) => {
+//  return $('h2')[0].innerHTML = a
+// }
+
+// texting('Testing')
+
+// ----------------------------- 2 -----------------------------
+/*
+Parašyti funkciją, kuri priima du kintamuosius.
+Patikrina ar jie yra skaičiai. Grąžina tų skaičių sumą.
+*/
+
+// const numbers = (a, b) => {
+//   let ans = 0;
+//   if (typeof a != "number" || typeof b != "number") {
+//     ans = console.log("Not numbers");
+//   } else {
+//     ans = a + b;
+//   }
+//   return ans;
+// };
+
+// console.log(numbers(4, 6));
+
+// ----------------------------- 3 -----------------------------
+/*
+Parašyti funkciją, kuri priima du kintamuosius. Patikrina ar jie yra skaičiai.
+Palygina tuos du skaičius ir grąžina rezultatą ar
+pirmas skaičius yra didesnis, ar antras, ar jie yra lygūs.
+*/
+
+// const numbers = (a, b) => {
+//   let ans = 0;
+//   if (typeof a != "number" || typeof b != "number") {
+//     ans = console.log("Not numbers");
+//   } else if (a > b){
+//     ans = a + ' yra didesnis (a)'
+//   } else if (a < b){
+//     ans = b + ' yra didesnis (b)'
+//   } else {
+//     ans = a + ' ir ' + b + ' yra lygus'
+//   }
+//   return ans;
+// };
+
+// console.log(numbers(7, 6));
+
+// ----------------------------- 4 -----------------------------
+/*
+Parašyti funkciją, kuri priimtų vieną kintamajį. Turi patikrinti
+ar tai yra skaičius ir ar tai yra keturženklis skaičius.
+Turi būti grąžinamas rezultatas ar tie metai yra keliemieji.
+*/
+
+// const numbers = (a) => {
+//   if (typeof a != "number" || a <= 1000) {
+//     return "Not numbers"
+//   } else if (a % 4 == 0 && a % 100 == 0){
+//     return 'Metai yra keliamieji: ' + a
+//   } else {
+//     return 'Metai nera keliamieji'
+//   }
+// };
+
+// console.log(numbers(2000));
+
+// ----------------------------- 5 -----------------------------
+/*
+Parašyti funkciją, kuri priimtų vieną kintamajį. Turi būti patikrinimas
+ar tas kintamasis yra skaičius. Funkcija turi grąžinti skaičių, kur
+įvesto skaičiaus skaitmenys būtų išrikiuoti didėjimo tvarka.
+*/
+
+// const numbers = (a) => {
+//   if (typeof a != "number") {
+//   return 'Ne skaicius'
+//   } else {
+//     return Number(a.toString().split('').sort((a, b) => a - b).join(''));
+//   }
+// };
+
+// console.log(numbers(189264));
+
+// ----------------------------- 6 -----------------------------
+/*
+Parašykite funkciją, kuri skaičiuotų, iš kiek sveikų skaičių jos argumentas
+dalijasi be liekanos (išskyrus vienetą ir patį save) Argumentą
+užrašykite taip, kad būtų galima įvesti tik sveiką skaičių.
+*/
+
+// const numbers = (a) => {
+//   if (typeof a != "number") {
+//       return 'Ne skaicius'
+//   } else if () {
+
+//   }
+// };
+
+// console.log(numbers(20));
+
+// ----------------------------- 7 -----------------------------
+/*
+Parašyti funkciją telefonoNumeris, kuri priima vieną kintamąjį.
+Turi būti patikrinimas, kad kintamasis yra masyvas, kad jo visi elementai
+yra skaičiai ir, kad jo ilgis yra 10. Funkcija turi grąžinti telefono numerį
+tokiu formatu - "(XXX) XXX-XXXX".
+*/
+
+// ----------------------------- 8 -----------------------------
+/*
+Parašyti funkciją pirminisSkaicius. Funkcija turi vieną kintamąjį. Turi būti patikrinimas,
+kad kintamasis yra skaičius. Funkcija turi grąžinti ar pateiktas skaičius yra pirminis( pirminis
+skaičius yra tas, kuris dalinasi tik iš savęs ir tik iš vieneto be liekanos.)
+*/
+
+// ----------------------------- 9 -----------------------------
+/*
+Parašyti funkciją - lygineSuma. Funkcijos parametrai - du kintamieji.
+Testų reikalavimai - abu kitamieji turi būti arba skaičiai arba masyva
+(negali būti vienas skaičius, kitas masyvas).
+Jei kintamieji skaičiai, grąžinti skaičių sumą, jei kintamieji masyvai -
+grąžinti masyvų ilgių sumą. Jei suma nelyginė - grąžinti tekstą, kad suma nelyginė.
+*/
+
+// ----------------------------- 10 -----------------------------
+/*
+Funkcija turi priimti du kintamuosius. Vienas turi būti skaičius, kitas masyvas su
+skaičiais ir minimum 7 elementais. Funkcija turi grąžinti true,
+jei skaičius yra masyve, ir false, jei nėra tokio skaičiaus masyve.
+*/
+
+const numbers = (a, b) => {
+  if (
+    (typeof a == "number" && Array.isArray(b) == true && b.length >= 7) ||
+    (typeof b == "number" && Array.isArray(a) == true && a.length >= 7)
+  ) {
+    for (let i = 0; i < a.length; i++) {
+      if (a.includes(b)) {
+        return "Skaicius yra " + b;
+      } else {
+        return "Nera " + b;
+      }
+    }
+    for (let i = 0; i < b.length; i++) {
+      if (b.includes(a)) {
+        return "Skaicius yra " + a;
+      } else {
+        return "Nera " + a;
+      }
+    }
+  } else {
+    return "BLOGAS SKAICIUS ARBA PER TRUMPAS MASYVAS";
+  }
+};
+
+console.log(numbers(2, [6, 1, 1, 0, 9, 8, 7]));
---
+++ Kestucio-uzdaviniu-viktorina-v5/assets/js/index.js
@@ -0,0 +1,318 @@
+function rndNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// ----------------------------- 1 -----------------------------
+/*
+Sugeneruoti masyvą, kurio ilgis 100, o reikšmės – masyvai,kurių
+ilgis 10, o reikšmės skaičiai nuo 1 iki 10 ([1,2,3,4,5,6,7,8,9,10]).
+*/
+
+// let arrayA = []
+// let arrayB = []
+// let arrayC = []
+
+// for (let i = 0; i < 100; i++) {
+//  arrayA.push(i)
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//  arrayB.push(i)-1
+// }
+
+// arrayA.forEach(element => {
+//   arrayC.push(arrayB)
+// });
+
+// console.log(arrayC);
+
+// ----------------------------- 2 -----------------------------
+/*
+Sugeneruoti masyvą, kurio ilgis 100, o reikšmės – masyvai, kurių
+ilgis 10, o reikšmės atsitiktiniai skaičiai nuo 1 iki 17.
+*/
+
+// let arrayA = [];
+
+// for (let i = 0; i < 100; i++) {
+//   arrayA.push(i);
+// }
+
+// arrayA.forEach((element) => {
+//   let arrayB = [];
+//   for (let i = 0; i < 10; i++) {
+//     let x = rndNum(1, 17);
+//     arrayB.push(x);
+//   }
+//   // console.log(arrayB);
+//   arrayA.push(arrayB);
+// });
+
+// arrayA.splice(0, 100);
+
+// console.log(arrayA);
+
+// ----------------------------- 3 -----------------------------
+/*
+Sugeneruoti masyvą, kurio ilgis 100, o reikšmės – masyvai, kurių
+ilgis atsitiktinis nuo 5 iki 18, o reikšmės – skaičiai
+nuo 1 iki masyvo ilgio.
+*/
+
+// let arrayA = [];
+
+// for (let i = 0; i < 100; i++) {
+//   arrayA.push(i);
+// }
+
+// arrayA.forEach((element) => {
+//   let arrayB = [];
+//   let y = rndNum(5, 18);
+//   for (let i = 0; i < y; i++) {
+//     arrayB.push(i);
+//   }
+//   // console.log(arrayB);
+//   arrayA.push(arrayB);
+// });
+
+// arrayA.splice(0, 100);
+
+// console.log(arrayA);
+
+// ----------------------------- 4 -----------------------------
+/*
+Sugeneruoti masyvą, kurio ilgis 100, o reikšmės – masyvai, kurių
+ilgis atsitiktinis nuo 5 iki 18, o reikšmės – atsitiktiniai
+skaičiai nuo 5 iki 30.
+*/
+
+// let arrayA = [];
+
+// for (let i = 0; i < 100; i++) {
+//   arrayA.push(i);
+// }
+
+// arrayA.forEach((element) => {
+//   let arrayB = [];
+//   let y = rndNum(5, 18);
+//   for (let i = 0; i < y; i++) {
+//     let x = rndNum(5, 30);
+//     arrayB.push(x);
+//   }
+//   // console.log(arrayB);
+//   arrayA.push(arrayB);
+// });
+
+// arrayA.splice(0, 100);
+
+// console.log(arrayA);
+
+// ----------------------------- 5 -----------------------------
+/*
+Sugeneruoti masyvą, kurio ilgis 100, o reikšmės – masyvai, kurių
+ilgis atsitiktinis nuo 5 iki 18, o reikšmės – atsitiktiniai skaičiai
+nuo 5 iki 75. Surasti didžiausią skaičių visame didžiajame masyve.
+*/
+
+// let arrayA = [];
+// let max = 0;
+
+// for (let i = 0; i < 100; i++) {
+//   arrayA.push(i);
+// }
+
+// arrayA.forEach((element) => {
+//   let arrayB = [];
+
+//   let y = rndNum(5, 18);
+//   for (let i = 0; i < y; i++) {
+//     let x = rndNum(5, 75);
+//     arrayB.push(x);
+//   }
+
+// arrayB.forEach(element => {
+//   if (element > max){
+//     max = element
+//   }
+// })
+//   arrayA.push(arrayB);
+// });
+
+// arrayA.splice(0, 100);
+
+// console.log('Max: ' + max);
+// console.log(arrayA);
+
+// ----------------------------- 6 -----------------------------
+/*
+Sugeneruoti masyvą, kurio ilgis 100, o reikšmės – masyvai, kurių
+ilgis atsitiktinis nuo 5 iki 18, o reikšmės – atsitiktiniai skaičiai
+nuo 5 iki 75. Surasti mažiausią skaičių visame didžiajame masyve ir
+kiek kartų jis pasikartojo.
+*/
+
+// let arrayA = [];
+// let min = 999999;
+// let count = 1;
+
+// for (let i = 0; i < 100; i++) {
+//   arrayA.push(i);
+// }
+
+// arrayA.forEach((element) => {
+//   let arrayB = [];
+
+//   let y = rndNum(5, 18);
+//   for (let i = 0; i < y; i++) {
+//     let x = rndNum(5, 75);
+//     arrayB.push(x);
+//   }
+
+//   arrayB.forEach((element) => {
+//     if (min == element) {
+//       count++;
+//     }
+//     if (element < min) {
+//       min = element;
+//     }
+//   });
+//   arrayA.push(arrayB);
+// });
+
+// arrayA.splice(0, 100);
+
+// console.log("Min: " + min);
+// console.log("Times: " + count);
+// console.log(arrayA);
+
+// ----------------------------- 7 -----------------------------
+/*
+Sugeneruoti masyvą, kurio ilgis 100, o reikšmės – masyvai, kurių
+ilgis 10, o reikšmės atsitiktiniai skaičiai nuo 1 iki 17.
+Išrykiuoti vidinių masyvų reikšmes didėjančia tvarka.
+*/
+
+// let arrayA = [];
+
+// for (let i = 0; i < 100; i++) {
+//   arrayA.push(i);
+// }
+
+// arrayA.forEach((element) => {
+//   let arrayB = [];
+
+//   for (let i = 0; i < 10; i++) {
+//     let x = rndNum(1, 17);
+//     arrayB.push(x);
+//   }
+//   arrayB.sort(e = (a, b) =>{
+//     return a - b;
+//   })
+//   arrayA.push(arrayB);
+// });
+
+// arrayA.splice(0, 100);
+
+// console.log(arrayA);
+
+// ----------------------------- 8 -----------------------------
+/*
+Sugeneruoti masyvą, kurio ilgis 100, o reikšmės – masyvai, kurių
+ilgis 10, o reikšmės atsitiktiniai skaičiai nuo 1 iki 17. Perrikiuoti
+vidinius masyvus pagal vidinių masyvų reikšmių sumas didėjančia tvarka.
+*/
+
+//NESIGAUNA
+
+// ----------------------------- 9 -----------------------------
+/*
+Sugeneruoti masyvą, kurio ilgis 100, o reikšmės – masyvai, kurių
+ilgis 10, o reikšmės atsitiktiniai skaičiai nuo 1 iki 17. Iš dvimačio
+masyvo sukurti vieną vienmatį masyvą – visas dvimačio masyvo masyvų
+reikšmes perkelti į naują masyvą.
+*/
+
+// let arrayA = [];
+// let arrayC = [];
+
+// for (let i = 0; i < 100; i++) {
+//   arrayA.push(i);
+// }
+
+// arrayA.forEach((element) => {
+//   let arrayB = [];
+//   for (let i = 0; i < 10; i++) {
+//     let x = rndNum(1, 17);
+//     arrayB.push(x);
+//   }
+//   arrayA.push(arrayB);
+//   arrayC.push(...arrayB)
+// });
+
+// arrayA.splice(0, 100);
+
+// console.log(arrayA);
+// console.log(arrayC);
+
+// ----------------------------- 10 -----------------------------
+/*
+Papildyti 29 uždavinį taip, kad surastų skaičių vidurkį ir palygintų
+jį su vidurinia reikšme, kuris didesnis. Jei elementų yra nelyginis
+skaičius, pvz 9, tai vidurinė reikšmė bus 5-as elementas, jei elementų
+yra lyginis skaičius, pvz 8, tai vidurinė reikšmė bus 4-os ir 5-os
+reikšmės aritmetinis vidurkis
+*/
+
+// let arrayA = [];
+// let arrayC = [];
+// let sum = 0;
+// let cycle = 0;
+// let avg = 0;
+// let vidSkc = 0;
+// let bendrasVid = 0;
+// let ats = 0;
+
+// for (let i = 0; i < 100; i++) {
+//   arrayA.push(i);
+// }
+
+// arrayA.forEach((element) => {
+//   let arrayB = [];
+//   let y = rndNum(9, 11);
+//   for (let i = 0; i < y; i++) {
+//     let x = rndNum(1, 17);
+//     arrayB.push(x);
+//   }
+//   arrayC.push(...arrayB);
+// });
+
+// console.log(arrayC);
+
+// arrayC.forEach((element) => {
+//   sum += element;
+//   cycle++;
+// });
+
+// bendrasVid = sum / cycle;
+
+// if (cycle % 2 != 0) {
+//   vidSkc = arrayC[(cycle + 1) / 2];
+// } else {
+//   vidSkc = (arrayC[cycle / 2] + arrayC[cycle / 2 + 1]) / 2;
+// }
+
+// if (bendrasVid > vidSkc) {
+//   ats = "Vidurkis yra didesnis, nei vidurinė reikšmė";
+// } else {
+//   ats = "Vidurinė reikšmė yra didesnė, nei vidurkis";
+// }
+
+// // console.log('Avg: ' + bendrasVid.toFixed(2));
+// // console.log('Sum: ' + sum);
+// // console.log('Cycle: ' + cycle);
+// // console.log('Mid ' + arrayC[cycle/2]);
+// // console.log('Mid ' + arrayC[vidSkc]);
+// // console.log('Mid only ' + vidSkc);
+// console.log(`Bendras vidurkis: ${bendrasVid.toFixed(2)}
+// Vidurinė reikšmė yra: ${vidSkc}
+// ${ats}`);
