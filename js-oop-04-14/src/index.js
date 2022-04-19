74811b45a8a742bcb416853611dd2096372b61ca
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-04-19 15:33:50 +0300

    Fixed app

---
+++ js-oop-04-14/src/index.js
@@ -0,0 +1,49 @@
+import User from './modules/user.js'
+console.log('testing')
+
+// let user = {
+//     name:['Vardenis', 'Pavardenis'],
+//     age:82,
+//     gender:'transgender',
+//     interests:['baseball', 'running', 'classical music'],
+//     greeting:function(){
+//         console.log(`Hi, I'm ${this.name[0]}`)
+//     },
+//     setAge:function(age){ //good practice
+//         this.age=age
+//     }
+// }
+
+// console.log(user.name)
+// user.age = 55; //bad practice
+// user.setAge(44) //good practice
+// user.greeting()
+
+// CONSTRUCTOR-------------------------------------------------------------------
+
+// function CreateUser(name, email, active){ //constructor starts with CAPITAL letter
+//         this.name = name
+//         this.email = email
+//         this.active = false
+// };
+
+// CreateUser.prototype.showName = function(){
+//     alert(`My name is ${this.name}`)
+// }
+
+// let userOne = new CreateUser('Mantelis', 'manTEL999@gmail.com')
+// userOne.showName()
+
+// printInfo();{
+//     console.log(`Hi, my name is ${this.name}. My e-mail is ${this.email}`)
+// }
+
+// console.log(userOne)
+
+
+// CLASSES (NEW) ------------------------------------------------------------------
+
+let userTwo = new User('Ben','fakemail@email.com','Tesla')
+
+userTwo.print()
+console.log('asd')
---
+++ js-oop-04-14/src/modules/user.js
@@ -0,0 +1,12 @@
+class User{
+    constructor(name, email, work){
+        this.name = name;
+        this.email = email;
+        this.work = work;
+    }
+    print(){
+        console.log(`Me - ${this.name}. I work ${this.work}. Contacts - ${this.email}`)
+    }
+}
+
+export default User
