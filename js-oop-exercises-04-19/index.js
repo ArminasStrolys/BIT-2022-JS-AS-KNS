94bf5780c00f28dcda927becf06cf0a130ae18b3
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-04-19 23:02:48 +0300

    Cat assignment with OOP

---
+++ js-oop-exercises-04-19/index.js
@@ -0,0 +1,22 @@
+import Cat from "./src/modules/cat.js"
+
+// let newCat = new Cat(`Tiredness: ${6}/10`, ` Hunger ${5}/10`, ` Loneliness ${5}/10`, `Happiness ${5}/10`)
+let newCat = new Cat(5,5,5,5)
+
+console.log('Cat starting levels ', newCat)
+
+newCat.feed()
+
+console.log('After feeding ', newCat)
+
+newCat.pet()
+
+console.log('After petting ', newCat)
+
+newCat.sleep(-13)
+
+console.log('After sleeping ', newCat)
+
+newCat.randomPet()
+
+newCat.status()
---
+++ js-oop-exercises-04-19/src/modules/cat.js
@@ -0,0 +1,53 @@
+function rndNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+class Cat {
+    
+  constructor(tiredness, hunger, lonliness, happiness) {
+    (this.tiredness = tiredness),
+      (this.hunger = hunger),
+      (this.lonliness = lonliness),
+      (this.happiness = happiness);
+  }
+  status() {
+    this.happiness > 10
+      ? console.log("Cat is happy!")
+      : this.hunger > 10
+      ? console.log("Cat is fat")
+      : this.lonliness > 10
+      ? console.log("Cat is sad")
+      : this.tiredness > 10
+      ? console.log("Cat is sleepy")
+      : console.log("Stats are at normal levels");
+  }
+  feed() {
+    (this.tiredness += 2),
+      (this.hunger -= 4),
+      (this.lonliness += 0),
+      (this.happiness += 3);
+  }
+  sleep(time) {
+    (this.tiredness -= 6),
+      (this.hunger += 2),
+      (this.lonliness += 0),
+      (this.happiness += time);
+  }
+  pet() {
+    (this.tiredness += 1),
+      (this.hunger += 0),
+      (this.lonliness -= 4),
+      (this.happiness += 2);
+  }
+  randomPet() {
+     let x = rndNum(1,10)
+    x >= 5 ? (
+    (this.tiredness += 1),
+      (this.hunger += 0),
+      (this.lonliness -= 4),
+      (this.happiness += 2)
+    ) : console.log('CAT DOES NOT WANT TO BE PETTED!')
+  }
+
+}
+export default Cat;
