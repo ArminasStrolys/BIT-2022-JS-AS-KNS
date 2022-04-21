b46a600929049dff95d2967a5a0ee9310eba49d9
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-04-21 21:55:20 +0300

    Days exercise - maps OOP

---
+++ js-oop-04-21/index.js
@@ -0,0 +1,11 @@
+// OOP ARRAYS 
+
+const map = new Map()
+
+map.set('indeksas', 10)
+
+console.log(map.get('indeksas')) // we get 10
+
+console.log(map.size) // we get array size
+
+console.log(map.delete('indeksas')) // we get true or false
