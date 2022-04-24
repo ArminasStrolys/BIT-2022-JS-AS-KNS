77f83dbb8cda5ae87ae370934b8d1a3cfbb2deaa
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-04-24 23:33:07 +0300

    Some progress

--- js-oop-basketball/index.js
+++ js-oop-basketball/index.js
@@ -2,3 +2,6 @@ import Basketball from "./modules/basketball.js";
 
 
 
+const homeBringers = new Basketball
+
+// homeBringers.init()
--- js-oop-basketball/modules/Basketball.js
+++ js-oop-basketball/modules/Basketball.js
@@ -1,11 +1,32 @@
-class Basketball{
-    constructor(){
-this.teamA = teamA
-this.teamB = teamB
-this.attackingTeam = this.teamA
-this.quarter = quarter
-this.match = match
-    }
+function rndNum(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+class Basketball{
+
+    static timer = 10
+    static qTime = 0
+
+    constructor(){
+this.teamA = 'Noobs'
+this.teamB = 'Pros'
+this.attackingTeam = this.teamA || this.teamB
+this.quarter = 1
+this.match = 'win'
+    }
+init(){
+    setTimeout( () => {
+        this.init()
+      }, 100)
+      console.log(Basketball)
+}
+getTimer(){}
+setClock(){}
+setQuarter(){}
+getQuarter(){}
+getScore(){}
+play(){}
+}
+
 
 export default Basketball
