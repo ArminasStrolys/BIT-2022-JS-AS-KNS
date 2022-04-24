f4e0e1e1c86a4c3f55d654a128205e23d88cb106
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-04-24 22:48:00 +0300

    Basket ball OOP starting files

---
+++ js-oop-basketball/index.js
@@ -0,0 +1,4 @@
+import Basketball from "./modules/basketball.js";
+
+
+
---
+++ js-oop-basketball/modules/Basketball.js
@@ -0,0 +1,11 @@
+class Basketball{
+    constructor(){
+this.teamA = teamA
+this.teamB = teamB
+this.attackingTeam = this.teamA
+this.quarter = quarter
+this.match = match
+    }
+}
+
+export default Basketball
