bd0e326501fff5d2d3f4844290d9cd6ae4a32722
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-04-25 10:43:37 +0300

    Progress

--- js-oop-04-25/index.js
+++ js-oop-04-25/index.js
@@ -1,20 +1,51 @@
-import readline from 'readline'
+import readline from "readline";
+import process from "process";
 
+// duomenu priemimo ir grazinimo konsoleje interface konfiguracija
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
+  input: process.stdin,
+  output: process.stdout,
+});
 
-rl.question('Enter your name: ', name => {
-    console.log('Your name is ' + name)
-    rl.close()
-})
+// vardo perdavimas ir grazinimas
+// rl.question('Enter your name: ', name => {
+//     console.log('Your name is ' + name)
+//     rl.close()
+// })
 
+// rl.question("Enter digit from 1 to 10: ", (digit) => {
+//   if (digit > 0 && digit <= 10) {
+//     for (let i = 1; i <= 10; i++) {
+//       console.log(digit + " x " + i + " = " + digit * i);
+//     }
+//   } else {
+//     console.log("Bad");
+//   }
+//   rl.close();
+// });
 
+// rl.question('Enter your name: ', name => {
+//     rl.question('Enter your surname: ', surname =>{
+//         console.log('Your full name is: \n' + name + ' ' + surname)
+//         rl.close()
+//     })
 
-// console.log('test')
+// })
 
-// let digit = 3
+rl.question("Enter num 1 (from): ", (a) => {
+  rl.question("Enter num 2 (to): ", (b) => {
+    console.log("kg. \t pound \t stone");
+    for (a; a <= b; a++) {
+      console.log(
+        `${parseInt(a)} \t ${parseFloat(a * 2.20462).toFixed(2)} \t ${parseFloat(
+          a * 0.157473
+        ).toFixed(2)} stone`
+      );
+    }
+    rl.close();
+  });
+});
+
 // for (let i = 1; i <= 10; i++){
 //     console.log(digit + ' x ' + i + ' = ' + (digit * i) )
 // }
