ad0c83c3796c5052ec483e170053f90cafd36d83
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-05-02 21:50:15 +0300

    Exercises #2 progress

--- js-express-05-02-exercises/index.js
+++ js-express-05-02-exercises/index.js
@@ -1,33 +1,63 @@
-import express from 'express'
-import { create } from 'express-handlebars';
-import { faker } from '@faker-js/faker';
+import express from "express";
+import { create } from "express-handlebars";
+import { faker } from "@faker-js/faker";
 
-//Express objekto inicijavimas
-const app = express()
-const hbs = create({ /* config */ });
-const address = faker.address.city()
-const car = faker.vehicle.vehicle()
-const name = faker.name.firstName()
-const photo = faker.image.avatar()
-const phone = faker.phone.phoneNumber()
-const workPlace = faker.company.companyName()
+import fs from "fs/promises";
+const app = express();
+const hbs = create({
+  /* config */
+});
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-app.set('views', './templates');
+const address = faker.address.city();
+const car = faker.vehicle.vehicle();
+const name = faker.name.firstName();
+const photo = faker.image.avatar();
+const phone = faker.phone.phoneNumber();
+const workPlace = faker.company.companyName();
 
-app.get('/', (req, res) => {
-    let variables = {
-        name: name,
-        address: address,
-        car: car,
-        photo: photo,
-        phone: phone,
-        workPlace: workPlace
-    }
-    res.render('card', variables)
-})
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+app.set("views", "./templates");
 
+app.get("/", (req, res) => {
+  let variables = {
+    name: name,
+    address: address,
+    car: car,
+    photo: photo,
+    phone: phone,
+    workPlace: workPlace,
+  };
+  res.render("card", variables);
+});
 
+app.get("/second", async (req, res) => {
+  //   let json = []
 
-app.listen(3000)
+  //       const data = fs.readFile('./data.json', 'utf8')
+
+  //       let parsedJson = JSON.parse(data)
+  // console.log(parsedJson)
+  //       parsedJson.push(req.query)
+
+  //       json = parsedJson
+
+  // console.log('hello')
+
+  //Informacijos issaugojimas faile
+  const data = await fs.readFile("./data.json", "utf8");
+let lat = [{}]
+
+  let parsedJson = JSON.parse(data);
+  parsedJson.map((e) => lat = e);
+  res.render("second", {lat});
+  console.log(lat)
+});
+
+app.listen(3000);
+
+// Panaudokite prisegtą JSON failą. Programiškai jį atsidarykite pasinaudodami filesystem nodejs moduliu.
+// Konvertuokite stringą į javascript masyvą ir perduokite duomenis į handlebars šabloną kuriame aprašykite
+// atvaizdavimo html kodą ir pozicijas kuriose turėtų atsidurti kintamieji.
+// Panaudokite if ir each funkcionalumą masyvo elementų atvaizdavimui.
+// Pasinaudokite inline css kodu, stilizuoti galutinį rezultatą.
