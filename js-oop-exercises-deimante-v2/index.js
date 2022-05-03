316e93f33a9a0233cac6ae9c92d7212c71b9f313
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-05-03 16:01:14 +0300

    Update index.js

--- js-oop-exercises-deimante-v2/index.js
+++ js-oop-exercises-deimante-v2/index.js
@@ -17,8 +17,6 @@ import Cup from "./modules/Cup.js";
 let a1 = Cup.createCup('RED', true);
 let a2 = Cup.createCup('YELLOW', false);
 console.log('================================')
-// Cup.fill(a1)
-// Cup.fill(a2)
 Cup.fill(a1, a2)
 console.log('================================')
 Cup.spillAll(a1, a2)
@@ -26,3 +24,5 @@ console.log('================================')
 Cup.fillAll(a1, a2)
 console.log('================================')
 Cup.fill(a1, a2)
+
+// #3 -----------------------------------------------------------------
